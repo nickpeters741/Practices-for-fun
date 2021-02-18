e5a2ba59a5c7aec51f6cb5d3019571a663cd1330
nickpeters741 <npeters741@gmail.com>
2021-02-18 12:58:01 +0300

    c code to make a square shape

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("_________________\n");
+    printf("|                |\n");
+    printf("|                |\n");
+    printf("|                |\n");
+    printf("|                |\n");
+    printf("|                |\n");
+    printf("|                |\n");
+    printf("_________________\n");
+    return 0;
+}
