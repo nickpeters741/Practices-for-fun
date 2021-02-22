518e50b38c5c553b8b3411ad2d8b2433a96155f0
nickpeters741 <npeters741@gmail.com>
2021-02-22 14:41:29 +0300

    algorithm defination practice in c

---
+++ algorithm.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main ()
+{
+         printf("algorithm is a set of instructions /rules to be followed in calculations or other problem-solving operations,especially by a computer\n");
+	 return 0;
+}
