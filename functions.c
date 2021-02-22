30e73aa1cb7554526526ea4e342e56b41f0e51a0
nickpeters741 <npeters741@gmail.com>
2021-02-22 14:52:03 +0300

    functions  defination practices

---
+++ functions.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("A function is a group of statements that together performs a task.A function declaration tells the compiler about a function's name,return type and parameters\n");
+    return 0;
+}
